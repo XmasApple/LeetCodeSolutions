a748d90f5af343e23950684ea8ff5686c3f74c43
XmasApple <y4blokof@yandex.ru>
2021-12-19 01:32:53 +0300

    10. Regular Expression Matching

---
+++ Problems/P10RegularExpressionMatching.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class P10RegularExpressionMatching
+    {
+        public readonly Dictionary<(int, int), bool> Matches = new();
+
+        public bool IsMatch(string s, string p, int si = 0, int pi = 0)
+        {
+            if (Matches.ContainsKey((si, pi)))
+                return Matches[(si, pi)];
+            if (p.Length == pi) return s.Length == si;
+            var firstMatch = s.Length > si && (p[pi] == s[si] || p[pi] == '.');
+
+            bool r;
+            if (p.Length > pi + 1 && p[pi + 1] == '*')
+                r = IsMatch(s, p, si, pi + 2) ||
+                    firstMatch && IsMatch(s, p, si + 1, pi);
+            else
+                r = firstMatch && IsMatch(s, p, si + 1, pi + 1);
+            Matches[(si, pi)] = r;
+            return r;
+        }
+    }
+}

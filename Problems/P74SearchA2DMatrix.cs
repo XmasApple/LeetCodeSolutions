1149b39a242f43dc1a3ecab5c6589bd2b4d500c1
XmasApple <y4blokof@yandex.ru>
2021-12-19 01:00:35 +0300

    74. Search a 2D Matrix

---
+++ Problems/P74SearchA2DMatrix.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems
+{
+    public class P74SearchA2DMatrix
+    {
+        public bool SearchMatrix(int[][] matrix, int target) => matrix.SelectMany(t => t).Any(t1 => t1 == target);
+
+        public bool SearchMatrixBinary(int[][] matrix, int target)
+        {
+            var m = matrix.Length;
+            var n = matrix[0].Length;
+            var potentialRow = -1;
+
+            for (var i = 0; i < m; i++)
+                if (target >= matrix[i][0])
+                    potentialRow = i;
+
+            if (potentialRow == -1)
+                return false;
+
+            for (var j = 0; j < n; j++)
+                if (matrix[potentialRow][j] == target)
+                    return true;
+
+            return false; 
+        }
+    }
+}

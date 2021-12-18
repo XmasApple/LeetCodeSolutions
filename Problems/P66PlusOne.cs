ef75e2386ce53ea4bebe96fb4c2d6924879802e7
XmasApple <y4blokof@yandex.ru>
2021-12-18 07:51:31 +0300

    66. Plus One

---
+++ Problems/P66PlusOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Problems
+{
+    public class P66PlusOne
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9)
+                    digits[i] = 0;
+                else
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
+            }
+
+            var r = new int[digits.Length + 1];
+            r[0] = 1;
+            for (var j = 0; j < digits.Length; j++)
+                r[j + 1] = digits[j];
+            return r;
+        }
+    }
+}

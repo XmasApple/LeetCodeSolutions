f9be910863d0fdf3da20da9282f9ba201d86c253
XmasApple <y4blokof@yandex.ru>
2021-12-19 17:03:13 +0300

    1832. Check if the Sentence Is Pangram

---
+++ Problems/P1832CheckIfTheSentenceIsPangram.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace LeetCode.Problems
+{
+    public class P1832CheckIfTheSentenceIsPangram
+    {
+        public bool CheckIfPangram(string sentence)
+        {
+            var flags = new bool[26];
+
+            foreach (var c in sentence)
+            {
+                flags[c - 'a'] = true;
+            }
+
+            return flags.All(_ => _);
+        }
+    }
+}

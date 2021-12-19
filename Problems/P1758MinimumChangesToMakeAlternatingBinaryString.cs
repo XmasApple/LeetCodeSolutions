fa7177bf834e16930476a5ad51e2834fbf39bf50
XmasApple <y4blokof@yandex.ru>
2021-12-19 16:51:59 +0300

    1758. Minimum Changes To Make Alternating Binary String

---
+++ Problems/P1758MinimumChangesToMakeAlternatingBinaryString.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Problems
+{
+    public class P1758MinimumChangesToMakeAlternatingBinaryString
+    {
+        public int MinOperations(string s)
+        {
+            var c0 = 0;
+            var c1 = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == (i % 2 == 0 ? '0' : '1'))
+                    c0 += 1;
+                else
+                    c1 += 1;
+            }
+
+            return c0 < c1 ? c0 : c1;
+        }
+    }
+}

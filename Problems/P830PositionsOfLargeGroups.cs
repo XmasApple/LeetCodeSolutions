3179d01064d7c187e3397cf2086929e03135133e
XmasApple <y4blokof@yandex.ru>
2021-12-19 16:39:03 +0300

    830. Positions of Large Groups

---
+++ Problems/P830PositionsOfLargeGroups.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class P830PositionsOfLargeGroups
+    {
+        public IList<IList<int>> LargeGroupPositions(string s)
+        {
+            var res = new List<IList<int>>();
+            var start = 0;
+            var t = '_';
+            for (var i = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                if (c != t)
+                {
+                    if (i - start >= 3)
+                        res.Add(new List<int> { start, i - 1 });
+                    t = c;
+                    start = i;
+                }
+            }
+
+            if (s.Length - start >= 3)
+                res.Add(new List<int> { start, s.Length - 1 });
+
+            return res;
+        }
+    }
+}

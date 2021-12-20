a24d836303b2315c7e8fc783b797dcf888e8c21d
XmasApple <y4blokof@yandex.ru>
2021-12-20 04:18:03 +0300

    3. Longest Substring Without Repeating Characters

---
+++ Problems/P3LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    public class P3LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var max = 0;
+            var cache = new int[256];
+            for (int i = 0, last = 0; i < s.Length; i++)
+            {
+                var c = s[i];
+                last = cache[c] == 0 ? last : last > cache[c] ? last : cache[c];
+                cache[c] = i + 1;
+                max = max > i - last + 1 ? max : i - last + 1;
+            }
+
+            return max;
+        }
+    }
+}

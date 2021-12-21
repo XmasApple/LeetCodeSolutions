3e2fd57463154d9ede691b3f0de0dad26428a4cd
XmasApple <y4blokof@yandex.ru>
2021-12-21 19:48:23 +0300

    Tests for Problem 1

--- Problems/P1TwoSum.cs
+++ Problems/P1TwoSum.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Problems
 {
-    public class P1TwoSum
+    public static class P1TwoSum
     {
-        public int[] TwoSum1(int[] nums, int target)
+        public static int[] TwoSum1(int[] nums, int target)
         {
             for (var i = 0; i < nums.Length; i++)
             {
@@ -17,7 +19,7 @@ namespace LeetCode.Problems
             return new int[2];
         }
 
-        public int[] TwoSum2(int[] nums, int target)
+        public static int[] TwoSum2(int[] nums, int target)
         {
             var dict = new Dictionary<int, int>();
             for (var i = 0; i < nums.Length; i++)
@@ -28,6 +30,53 @@ namespace LeetCode.Problems
             }
 
             return new int[2];
+        }
+
+        private static readonly ((int[], int), int[])[] TestPairs = new[]
+        {
+            ((new[] { 2, 7, 11, 15 }, 9), new[] { 0, 1 }),
+            ((new[] { 3, 2, 4 }, 6), new[] { 1, 2 }),
+            ((new[] { 3, 3 }, 6), new[] { 0, 1 }),
+        };
+
+        public static void Test1()
+        {
+            for (int i = 0; i < TestPairs.Length; i++)
+            {
+                var ((item1, item2), expected) = TestPairs[i];
+
+                var result = TwoSum1(item1, item2);
+                if (result.SequenceEqual(expected))
+                    Console.WriteLine($"Test P1TwoSum1 #{i + 1} passed");
+                else
+                {
+                    Console.WriteLine($"Test P1TwoSum1 #{i + 1} failed");
+                    Console.WriteLine("Expected:");
+                    Console.WriteLine($"[{string.Join(", ", expected)}");
+                    Console.WriteLine("Given:");
+                    Console.WriteLine($"[{string.Join(", ", result)}");
+                }
+            }
+        }
+
+        public static void Test2()
+        {
+            for (int i = 0; i < TestPairs.Length; i++)
+            {
+                var ((item1, item2), expected) = TestPairs[i];
+
+                var result = TwoSum2(item1, item2);
+                if (result.SequenceEqual(expected))
+                    Console.WriteLine($"Test P1TwoSum2 #{i + 1} passed");
+                else
+                {
+                    Console.WriteLine($"Test P1TwoSum2 #{i + 1} failed");
+                    Console.WriteLine("Expected:");
+                    Console.WriteLine($"[{string.Join(", ", expected)}");
+                    Console.WriteLine("Given:");
+                    Console.WriteLine($"[{string.Join(", ", result)}");
+                }
+            }
         }
     }
 }

38a52f3673fa71bc90827772322c8bbb8b8d24b8
XmasApple <y4blokof@yandex.ru>
2021-12-21 22:06:25 +0300

    Tests for Problem 1758

--- Problems/P1758MinimumChangesToMakeAlternatingBinaryString.cs
+++ Problems/P1758MinimumChangesToMakeAlternatingBinaryString.cs
@@ -1,8 +1,12 @@
-﻿namespace LeetCode.Problems
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace LeetCode.Problems
 {
-    public class P1758MinimumChangesToMakeAlternatingBinaryString
+    public static class P1758MinimumChangesToMakeAlternatingBinaryString
     {
-        public int MinOperations(string s)
+        public static int MinOperations(string s)
         {
             var c0 = 0;
             var c1 = 0;
@@ -15,6 +19,33 @@
             }
 
             return c0 < c1 ? c0 : c1;
+        }
+        private static readonly (string, int)[] TestPairs =
+        {
+            ("0100", 1),
+            ("10", 0),
+            ("1111", 2),
+        };
+
+        public static void Test()
+        {
+            var name = MethodBase.GetCurrentMethod()?.DeclaringType?.Name.Split('.').Last();
+            for (var i = 0; i < TestPairs.Length; i++)
+            {
+                var (s, expected) = TestPairs[i];
+
+                var result = MinOperations(s);
+                if (result==expected)
+                    Console.WriteLine($"Test {name} #{i + 1} passed");
+                else
+                {
+                    Console.WriteLine($"Test {name} #{i + 1} failed");
+                    Console.WriteLine("Expected:");
+                    Console.WriteLine(expected);
+                    Console.WriteLine("Given:");
+                    Console.WriteLine(result);
+                }
+            }
         }
     }
 }

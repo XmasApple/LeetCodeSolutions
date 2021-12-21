c253debec47908f61fa944346c06b56889fdf237
XmasApple <y4blokof@yandex.ru>
2021-12-21 23:18:13 +0300

    338. Counting Bits

---
+++ Problems/P338CountingBits.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace LeetCode.Problems
+{
+    public static class P338CountingBits
+    {
+        public static int[] CountBits(int n)
+        {
+            var r = new int[n + 1];
+            // for (var i = 0; i <= n; i++)
+            // {
+            //     var current = i;
+            //     for (var j = 0; j < 32; j++)
+            //     {
+            //         if ((current & 1) == 1) r[i] += 1;
+            //         current >>= 1;
+            //     }
+            // }
+            for (var i = 1; i <= n; i++)
+                r[i] = r[i >> 1] + (i & 1);
+            
+            return r;
+        }
+
+        private static readonly (int, int[])[] TestPairs =
+        {
+            (2, new[] { 0, 1, 1 }),
+            (5, new[] { 0, 1, 1, 2, 1, 2 }),
+        };
+
+        public static void Test()
+        {
+            var name = MethodBase.GetCurrentMethod()?.DeclaringType?.Name.Split('.').Last();
+            for (var i = 0; i < TestPairs.Length; i++)
+            {
+                var (s, expected) = TestPairs[i];
+
+                var result = CountBits(s);
+                if (result.SequenceEqual(expected))
+                    Console.WriteLine($"Test {name} #{i + 1} passed");
+                else
+                {
+                    Console.WriteLine($"Test {name} #{i + 1} failed");
+                    Console.WriteLine("Expected:");
+                    Console.WriteLine(expected);
+                    Console.WriteLine("Given:");
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
